497d206182a63c632e8ce041322340a9d58ca278
mumei <vennapoosasurya@gmail.com>
2021-05-11 00:37:51 +0530

    final commit

--- MovieTicketBookingApplication-BackEnd/MTB-App/src/main/java/com/cg/mts/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cg.mts.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.mts.pojo.Ticket;
-
-public interface TicketRepository extends JpaRepository<Ticket, Integer> {
-
-}
--- MovieTicketBookingApplication-BackEnd/MTB/src/main/java/com/cg/mts/repository/TheatreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cg.mts.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.mts.pojo.Theatre;
-
-public interface TheatreRepository extends JpaRepository<Theatre, Integer> {
-
-}
